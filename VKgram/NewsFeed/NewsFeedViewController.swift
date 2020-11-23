49601ac4f2b7fc98069ec8dff6eb336a345a00bb
anod3v <anod3v@gmail.com>
2020-11-23 22:00:53 +0300

    Lesson 7 done

--- VKgram/NewsFeed/NewsFeedViewController.swift
+++ VKgram/NewsFeed/NewsFeedViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PromiseKit
 
 class NewsFeedViewController: UIViewController, NewsFeedTableViewCellDelegate {
     
@@ -33,9 +34,20 @@ class NewsFeedViewController: UIViewController, NewsFeedTableViewCellDelegate {
         return tableView
     }()
     
+    var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshNews), for: .valueChanged)
+        
+        return refreshControl
+    }()
+    
     let cellID = "NewsFeedTableViewCell"
     
     let headerID = "NewsFeedCollectionViewHeader"
+    
+    var nextFrom = ""
+    
+    var isLoading = false
     
     
     
@@ -44,20 +56,42 @@ class NewsFeedViewController: UIViewController, NewsFeedTableViewCellDelegate {
         
         networkService.getNewsFeedItems()?.done { result in
             self.handleGetNewsFeedResponse(item: result.0.response, profile: result.1.response, group: result.2.response)
-       }
+        }
         
         view.addSubview(tableView)
         
         tableView.delegate = self
         tableView.dataSource = self
+        //        tableView.prefetchDataSource = self
         tableView.register(NewsFeedTableViewCell.self, forCellReuseIdentifier: cellID)
         tableView.pin(to: view)
+
     }
+    
+    @objc func refreshNews() {
+        
+        self.refreshControl.beginRefreshing()
+        networkService.getNewsFeedItems()?.done { result in
+            self.handleGetNewsFeedResponse(item: result.0.response, profile: result.1.response, group: result.2.response)
+        }
+    }
+    
+    
     
     func handleGetNewsFeedResponse(item: ItemResponse, profile: ProfileResponse, group: GroupResponse) {
         self.newFeedItems = item.items
         self.newsFeedProfiles = profile.profiles
         self.newsFeedGroups = group.groups
+        self.nextFrom = item.nextFrom
+        
+        DispatchQueue.main.async { self.tableView.reloadData() }
+    }
+    
+    func handleGetNewsFeedNextResponse(item: ItemResponse, profile: ProfileResponse, group: GroupResponse) {
+        self.newFeedItems.append(contentsOf: item.items)
+        self.newsFeedProfiles.append(contentsOf: profile.profiles)
+        self.newsFeedGroups.append(contentsOf: group.groups)
+        self.nextFrom = item.nextFrom
         
         DispatchQueue.main.async { self.tableView.reloadData() }
     }
@@ -96,10 +130,38 @@ extension NewsFeedViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard !isLoading else { return }
+        
+        if indexPath.row == newFeedItems.count - 1 {
+            let lastIndex = indexPath.row
+            isLoading = true
+            firstly {
+                networkService.getNewsFeedItemsWithStartTime(startFrom: nextFrom)!
+            }.done { (result) in
+                
+                self.handleGetNewsFeedNextResponse(item: result.0.response, profile: result.1.response, group: result.2.response)
+                
+                let indexPaths = self.makeIndexSet(lastIndex: lastIndex, result.0.response.items.count)
+                tableView.insertRows(at: indexPaths, with: .automatic)
+                self.isLoading = false
+            }.catch { (error) in
+                debugPrint(error.localizedDescription)
+                self.isLoading = false
+            }
+        }
+    }
+    
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         
         return cell.bounds.size.height
     }
     
+    private func makeIndexSet(lastIndex: Int, _ newsCount: Int) -> [IndexPath] {
+        let last = lastIndex + newsCount
+        let indexPaths = Array(lastIndex + 1...last).map { IndexPath(row: $0, section: 0) }
+        
+        return indexPaths
+    }
     
 }
